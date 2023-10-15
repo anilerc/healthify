1b5a216cff148005555701af7d4ccd4c2929781c
anilerc <anilerc17@gmail.com>
2023-10-15 11:24:12 +0300

    Initial commit

---
+++ src/test/java/com/anilerc/healthify/HealthifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anilerc.healthify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
